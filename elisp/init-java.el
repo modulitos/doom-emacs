e78819aa5ec3f47b81a64badac2333f4370825c6
modulitos <hi@modulitos.com>
2024-11-17 21:35:50 -0800

    updates

---
+++ elisp/init-java.el
@@ -0,0 +1,16 @@
+;;; elisp/init-java.el -*- lexical-binding: t; -*-
+
+;; (use-package java-mode
+;;   ;; :mode (("\\.java$" . java-mode))
+;;   :config
+;;   (defun my-java-mode-hook ()
+;;     (message "inside java mode 2")
+;;     (local-set-key (kbd "M-b") 'better-jumper-jump-backward)
+;;     (local-set-key (kbd "M-RET") '+lookup/definition)
+;;     )
+
+;;   :hook ((java-mode . my-java-mode-hook))
+;;   )
+
+(provide 'init-java)
+;;; init-java.el ends here
